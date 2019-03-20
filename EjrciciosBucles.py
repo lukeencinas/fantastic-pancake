378f7f70b3ddf8cd9cba7349a2e2c5f917514115
Edu Banner <Edu.B@gmail.com>
2019-03-20 12:03:50 +0100

    Añadido clase python

---
+++ EjrciciosBucles.py
@@ -0,0 +1,27 @@
+#1)  for i in range(10):
+#    print("Lucas")
+
+
+#2) edad = int(input("Cual es tu edad? "))
+#for i in range(edad):
+ #   print(i + 1)
+
+
+#3) numero = int(input("Escribe un numero positivo: "))
+#while (numero <= 0):
+ #   numero = int(input("Escribe un POSITIVO: "))
+#if (numero >= 0):
+ #   for i in range(numero):
+   #     if i%2 == 1:
+  #          print(i,end=', ')
+
+
+
+#4) numero = int(input("Escribe un numero positivo"))
+#while (numero <= 0):
+ #   numero = int(input("Escribe un POSITIVO: "))
+#for i in range(numero):
+ # print (numero -i, end=', ')
+
+
+
