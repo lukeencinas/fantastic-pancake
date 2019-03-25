bc628dfc75e516eda4156111ca3d0926635f7a4c
Edu Banner <Edu.B@gmail.com>
2019-03-25 12:56:41 +0100

    Commit

--- Listas.py
+++ Listas.py
@@ -1,3 +1,21 @@
-a = [1,2,3,4]
-type(a)
-a.append(5)
+#1) e1 = ["F. de Computadores", "Visualizacion","Fisica","Quimica", "Historia", "Lengua"]
+# print(e1)
+
+#2) e2 = ["F. de Computadores", "Visualizacion","Fisica","Quimica", "Historia", "Lengua"]
+# for i in e2:
+  #  print("Yo estudio: "+i)
+
+#3) e3 = ["F. de Computadores", "Visualizacion","Fisica"]
+# nota = []
+# for i in e3:
+  #  nota.append(input('¿Que has sacado en '+ i + '? '))
+# for i in range(len(e3)):
+ #   print ("En la asignatura ",e3[i]," Has sacado ",nota[i])
+
+
+#4) e4 = []
+# for i in range(6):
+  #  e4.append(int(input("¿Cuales son los numeros de la loteria? ")))
+# e4.sort()
+# print("Los numeros en orden son: "+ str(e4))
+
